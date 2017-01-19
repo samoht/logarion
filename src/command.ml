8ca7559a882ebddc3725eb27fbd07b6d9a23ba6c
Stavros Polymenis <sp@orbitalfox.com>
2017-01-19 23:10:04 +0000

    init command implementation

--- src/command.ml
+++ src/command.ml
@@ -1,4 +1,29 @@
 open Cmdliner
+module C = Logarion.Configuration
+
+let init =
+  let f force =
+    let repo =
+      C.((try of_filename "logarion.toml" with Sys_error _ -> default ()).repository)
+      |> Logarion.repodir_string
+    in
+    let make_dir d =
+      let open Unix in
+      try mkdir d 0o700
+      with Unix_error (EEXIST, "mkdir", _) -> prerr_endline @@ "Already exists: " ^ d
+    in 
+    if not force && Array.length (Sys.readdir repo) > 0 then
+      prerr_endline "Directory is it not empty. Call with -f to init anyway."
+    else
+      List.iter make_dir ["title"; "uuid"];
+  in
+  let force =
+    Arg.(value & flag & info ["f"; "force"] ~doc:"Initialise repository even if directory is non empty")
+  in
+  Term.(const f $ force),
+  Term.info
+    "init" ~doc:"initialise a logarion repository in present directory"
+    ~man:[ `S "DESCRIPTION"; `P "Create a repository in current directory" ]
 
 let create =
   let title =
@@ -26,7 +51,7 @@ let default_cmd =
   Term.(ret (const (`Help (`Pager, None)))),
   Term.info "logarion" ~version:"0.1.0" ~doc ~man
 
-let cmds = [ create; ]
+let cmds = [ init; create ]
 
 let () = match Term.eval_choice default_cmd cmds with
   | `Error _ -> exit 1 | _ -> exit 0
